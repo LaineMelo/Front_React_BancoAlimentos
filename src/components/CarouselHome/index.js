917711ace71a858f44477dc020f27ecce2c374b2
Elaine Melo <elaine.melo.1353338@sga.pucminas.br>
2023-04-27 18:08:53 -0300

    modificando interface Home

--- src/components/CarouselHome/index.js
+++ src/components/CarouselHome/index.js
@@ -3,21 +3,25 @@ import agricultura from '../../img/imgagrf1.jpg'
 import bmaslide from '../../img/imgagrf3.jpg'
 import cestasverdes from '../../img/imgagrf2.jpg'
 import styles from './CarouselHome.module.css'
+import imgSistema from '../../img/imgSistema.png'
 
 function CarouselHome() {
     return (
-        <div className={styles.carouselInner}>
-                <Carousel  indicators={false}>
-                    <Carousel.Item >
-                        <img className={styles.img} src={agricultura} alt="Agricultura Familiar" />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img className={styles.img} src={bmaslide} alt="BMA" />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img className={styles.img} src={cestasverdes} alt="Cestas Verdes" />
-                    </Carousel.Item>
-                </Carousel>
+        <div>
+            <img src={imgSistema} className={styles.imgTop}></img>
+            <div className={styles.carouselInner}>
+                    <Carousel  indicators={false}>
+                        <Carousel.Item >
+                            <img className={styles.img} src={agricultura} alt="Agricultura Familiar" />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img className={styles.img} src={bmaslide} alt="BMA" />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img className={styles.img} src={cestasverdes} alt="Cestas Verdes" />
+                        </Carousel.Item>
+                    </Carousel>
+            </div>
         </div>
     )
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,14 @@
 import CarouselHome from '../../components/CarouselHome/index'
 import Cards from '../../components/Cards/index'
-import solidarity from '../../img/solidarity.png'
-import styles from './Home.module.css'
-import imgSistema from '../../img/imgSistema.png'
+//import solidarity from '../../img/solidarity.png'
+//import styles from './Home.module.css'
+//import imgSistema from '../../img/imgSistema.png'
 
 
 function Home() {
     return (
         <div >
             <div> 
-                <img src={imgSistema} className={styles.imgTop}></img>     
                 <CarouselHome />
             </div>
             <div>
